948b2f973fbcba10dbf229186d294e43be03ded0
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-22 16:58:34 +0100

    wrap with asyncHandler

--- src/controllers/games.controller.ts
+++ src/controllers/games.controller.ts
@@ -1,11 +1,9 @@
 import type { Request, Response, NextFunction } from 'express';
+import { NotFoundError } from '@/utils/errors';
+import { asyncHandler } from '@/utils/asyncHandler';
 import * as Game from '../models/game.model';
 
-export const getAllGames = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const allGames = await Game.getAll();
-    res.json({ allGames });
-  } catch (error) {
-    next(error);
-  };
-}
+export const getAllGames = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  const allGames = await Game.getAll();
+  res.json({ allGames });
+});
