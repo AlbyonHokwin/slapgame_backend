42bd6530745e5cf022637cb082165ed90e820c81
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-22 16:57:56 +0100

    wrapper to handle errors from promise

---
+++ src/utils/asyncHandler.ts
@@ -0,0 +1,3 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const asyncHandler = (fn: any) => (req: Request, res: Response, next: NextFunction) => Promise.resolve(fn(req, res, next)).catch(next);
