f831790f39f5bbd1ecf1bcec124d7eb74e2f6b4c
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-22 15:52:27 +0100

    add try catch

--- src/controllers/games.controller.ts
+++ src/controllers/games.controller.ts
@@ -1,7 +1,11 @@
-import type { Request, Response } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 import * as Game from '../models/game.model';
 
-export const getAllGames = async (req: Request, res: Response) => {
-  const allGames = await Game.getAll();
-  res.json(allGames);
+export const getAllGames = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const allGames = await Game.getAll();
+    res.json({ allGames });
+  } catch (error) {
+    next(error);
+  };
 }
