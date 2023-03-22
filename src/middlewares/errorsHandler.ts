56afec2abdb1b7c32fded7a3294ea78e60b3f1d6
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-22 16:58:15 +0100

    handle custom errors

--- src/middlewares/errorsHandler.ts
+++ src/middlewares/errorsHandler.ts
@@ -1,11 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
+import { BaseError } from '@/utils/errors';
 
 const errorsHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
   if (res.headersSent) {
     next(err);
   }
 
-  return res.status(500).json({ error: 'Erreur interne' });
+  console.log('here :', err);
+
+  if (err instanceof BaseError) {
+    return res.status(err.statusCode).json({ error: err.message })
+  }
+
+  return res.status(500).json({ error: 'Internal error' });
 }
 
 export default errorsHandler;
