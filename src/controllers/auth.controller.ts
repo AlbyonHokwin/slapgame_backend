fc67669fc56911f24b0a31bf55f734fea4c462f9
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-24 15:31:43 +0100

    can create new user

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,14 +1,33 @@
-import type { Request, Response, NextFunction } from 'express';
 import { asyncHandler } from '@/utils/asyncHandler';
+import * as User from '../models/user.model';
 
-export const signup = asyncHandler((req: Request, res: Response) => {
-  res.json('signup');
+import type { Request, Response } from 'express';
+import type { UserWithPwd } from '@@/types/User';
+import { ConflictRequestError } from '@/utils/errors';
+
+export const signup = asyncHandler(async (req: Request, res: Response) => {
+  const { username, email, password } = req.body;
+  const newUser: UserWithPwd = { username, email, password };
+  const isEmailAlreadyUsed = await User.emailAlreadyUsed(email);
+
+  if (isEmailAlreadyUsed) {
+    throw new ConflictRequestError('Email is already used');
+  }
+
+  const createdUser = await User.create(newUser);
+
+  if (createdUser.id >= 0) {
+    res.status(201).json({ result: { username: createdUser.username, email: createdUser.email } });
+    return;
+  }
+
+  throw new Error('Creation of new user failed');
 });
 
-export const signin = asyncHandler((req: Request, res: Response) => {
+export const signin = asyncHandler(async (req: Request, res: Response) => {
   res.json('signin');
 });
 
-export const refreshToken = asyncHandler((req: Request, res: Response) => {
+export const refreshToken = asyncHandler(async (req: Request, res: Response) => {
   res.json('refreshToken');
 });
