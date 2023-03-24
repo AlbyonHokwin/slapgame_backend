20d651c363785b91e6f9b632f8a9f2a536e8736d
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-24 14:54:04 +0100

    create middleware

---
+++ src/validation/validate.ts
@@ -0,0 +1,15 @@
+import type { Request, Response, NextFunction } from 'express';
+import { FailedValidationError } from '@/utils/errors';
+import { type ValidationChain, validationResult } from 'express-validator';
+
+export const validate = (validations: ValidationChain[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    console.log('validate', req.body);
+    await Promise.all(validations.map(validation => validation.run(req)));
+
+    const errors = validationResult(req);
+    if (errors.isEmpty()) return next();
+
+    next(new FailedValidationError(errors.array()));
+  };
+};
