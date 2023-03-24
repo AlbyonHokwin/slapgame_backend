a6264f8347bb214731aea291b5971b77b1391e0c
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-24 13:23:29 +0100

    add type with required field

--- types/User.d.ts
+++ types/User.d.ts
@@ -1,5 +1,14 @@
-export type User = {
+export interface User {
+  id?: number;
   username: string;
   email: string;
+  password?: string;
+}
+
+export interface UserWithPwd extends User {
   password: string;
-};
+}
+
+export interface UserWithId extends User {
+  id: number;
+}
