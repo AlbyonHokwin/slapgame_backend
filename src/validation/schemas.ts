33eaadde951c92aa4b61b2ded01074897274ba95
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-27 12:37:44 +0200

    add userSchema

--- src/validation/schemas.ts
+++ src/validation/schemas.ts
@@ -1,19 +1,19 @@
-import type { Request } from 'express';
-import { BadRequestError } from '@/utils/errors';
 import { type ValidationChain, body } from 'express-validator';
+import { checkEmail, checkPassword } from './checks';
 
 export const addUserSchema: ValidationChain[] = [
   body('username').exists().withMessage('No username provided').bail()
     .trim().isLength({ min: 3 }).withMessage('Username must be at least 3 characters').bail()
     .isAlphanumeric(undefined, { ignore: ' -.' }).withMessage('Can only contains alphanumeric characters with whitespaces and -'),
 
-  body('email').exists().withMessage('No email provided').bail()
-    .trim().isEmail().withMessage('Add correct email').bail()
-    .normalizeEmail(),
-
-  body('password').exists().withMessage('No password provided').bail()
-    .isLength({ min: 3 }).withMessage('Password needs to be at least 3 characters'),
+  checkEmail,
+  checkPassword,
 
   body('passwordConfirmation').exists().withMessage('No confirmation of password provided').bail()
     .custom((value, { req }) => value === req.body.password).withMessage('Password confirmation does not match password'),
+];
+
+export const userSchema: ValidationChain[] = [
+  checkEmail,
+  checkPassword,
 ];
