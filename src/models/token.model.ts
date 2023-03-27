fdf5b2e0cee7165cf656abe7abb5de265b2a6650
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-27 16:18:47 +0200

    add getOne function

--- src/models/token.model.ts
+++ src/models/token.model.ts
@@ -2,6 +2,7 @@ import { db } from './db';
 import { v4 as uuidv4 } from 'uuid';
 
 import type { UserWithId } from '@@/types/User';
+import type { RefreshToken } from '@@/types/RefreshToken';
 
 export const create = async (id: UserWithId['id']): Promise<string> => {
   const token: string = uuidv4();
@@ -21,27 +22,27 @@ export const create = async (id: UserWithId['id']): Promise<string> => {
   }
 };
 
-// Not tested
-export const verify = async (token: string, id: UserWithId['id']): Promise<boolean> => {
-  const statement = 'SELECT token, user_id, usable, expires_at FROM tokens WHERE token = $1 AND user_id = $2 AND usable = true';
-  const values = [token, id];
+export const getOne = async (token: string): Promise<RefreshToken | null> => {
+  const statement = 'SELECT token, user_id, usable, expires_at FROM tokens WHERE token = $1';
+  const values = [token];
 
   try {
     const results = await db.query(statement, values);
 
-    if (results.rows.length === 0) return false;
+    if (results.rows.length === 0) return null;
     const tokenData = results.rows[0];
 
-    const today: Date = new Date();
-    const expiresAt: Date = new Date(tokenData.expires_at);
-
-    return today > expiresAt;
+    return {
+      token: `${tokenData.token}`,
+      user_id: parseInt(tokenData.user_id, 10),
+      usable: tokenData.usable,
+      expires_at: new Date(tokenData.expires_at),
+    };
   } catch (error) {
     throw error;
   }
 };
 
-// Not tested
 export const revokeOne = async (token: string): Promise<void> => {
   const statement = 'UPDATE tokens SET usable = false WHERE token = $1';
   const values = [token];
@@ -54,7 +55,6 @@ export const revokeOne = async (token: string): Promise<void> => {
   }
 };
 
-// Not tested
 export const revokeAllForId = async (id: UserWithId['id']): Promise<void> => {
   const statement = 'UPDATE tokens SET usable = false WHERE user_id = $1';
   const values = [id];
