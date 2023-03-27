ed2e45eb9723512a7502c3bd88934e50b746fe96
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-27 13:01:26 +0200

    add validation for signin route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { validate } from '@/validation/validate';
-import { addUserSchema } from '@/validation/schemas';
+import { addUserSchema, userSchema } from '@/validation/schemas';
 import { signup, signin, refreshToken } from '@/controllers/auth.controller';
 
 const router = Router();
 
 router.post('/signup', validate(addUserSchema), signup);
-router.post('/signin', signin);
+router.post('/signin', validate(userSchema), signin);
 router.post('/refresh', refreshToken);
 
 export default router;
