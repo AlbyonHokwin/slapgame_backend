28c3354cb2b21d4cdc2932994a7b057c7cc43356
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-27 17:44:53 +0200

    add signout route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { validate } from '@/validation/validate';
-import { addUserSchema, refreshTokenSchema, userSchema } from '@/validation/schemas';
-import { signup, signin, refreshToken } from '@/controllers/auth.controller';
+import { signupSchema, signinSchema, signoutSchema, refreshTokenSchema } from '@/validation/schemas';
+import { signup, signin, signout, refreshToken } from '@/controllers/auth.controller';
 
 const router = Router();
 
-router.post('/signup', validate(addUserSchema), signup);
-router.post('/signin', validate(userSchema), signin);
+router.post('/signup', validate(signupSchema), signup);
+router.post('/signin', validate(signinSchema), signin);
+router.post('/signout', validate(signoutSchema), signout);
 router.post('/refresh', validate(refreshTokenSchema), refreshToken);
 
 export default router;
