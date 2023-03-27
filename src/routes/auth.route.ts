8f323f792637fd6d9283ee1a32f3bfd7fa75a09a
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-27 16:19:00 +0200

    add validation for refresh

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { validate } from '@/validation/validate';
-import { addUserSchema, userSchema } from '@/validation/schemas';
+import { addUserSchema, refreshTokenSchema, userSchema } from '@/validation/schemas';
 import { signup, signin, refreshToken } from '@/controllers/auth.controller';
 
 const router = Router();
 
 router.post('/signup', validate(addUserSchema), signup);
 router.post('/signin', validate(userSchema), signin);
-router.post('/refresh', refreshToken);
+router.post('/refresh', validate(refreshTokenSchema), refreshToken);
 
 export default router;
