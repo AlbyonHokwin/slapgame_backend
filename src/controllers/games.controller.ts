9e61ef567cb0ab27404995544f511bb3dc3f3237
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-29 18:38:15 +0200

    start creating controller for new game

--- src/controllers/games.controller.ts
+++ src/controllers/games.controller.ts
@@ -1,7 +1,24 @@
 import type { Request, Response, NextFunction } from 'express';
-import { NotFoundError } from '@/utils/errors';
+import { body, validationResult } from 'express-validator';
 import { asyncHandler } from '@/utils/asyncHandler';
+import { FailedValidationError } from '@/utils/errors';
 import * as Game from '../models/game.model';
+
+export const createGame = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  let { hostId, cardDeckId, penaltyPrice, strikeNumber, isPrivate, password } = req.body;
+
+  if (isPrivate) {
+    await body('password').exists().withMessage('Password is required for private game').bail()
+      .isLength({ min: 3 }).withMessage('Password needs to be at least 3 characters').run(req);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) next(new FailedValidationError(errors.array()));
+  } else password = null;
+
+  const createdGame = await Game.create({ hostId, cardDeckId, penaltyPrice, strikeNumber, isPrivate, password });
+  console.log(createdGame);
+
+  res.json('createGame');
+});
 
 export const getAllGames = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   const allGames = await Game.getAll();
