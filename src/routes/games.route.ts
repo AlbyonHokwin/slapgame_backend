ae6815bd8315a32d095544fc9b4a98fa1b2dc9f1
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-29 18:37:29 +0200

    create "new" route

--- src/routes/games.route.ts
+++ src/routes/games.route.ts
@@ -1,12 +1,10 @@
-import { Request, Response, Router } from 'express';
-import { getAllGames } from '../controllers/games.controller';
+import { Router } from 'express';
+import { validate } from '@/validation/validate';
+import { createGameSchema } from '@/validation/games.schema';
+import { createGame } from '../controllers/games.controller';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.json('ok');
-});
-
-router.get('/all', getAllGames);
+router.post('/new', validate(createGameSchema), createGame);
 
 export default router;
