187fcb3332f0c046227c8f821002e08c8e7c4681
Camille HAUSTANT <camillehaustant@gmail.com>
2023-04-05 15:47:11 +0200

    formatting

--- types/Card.d.ts
+++ types/Card.d.ts
@@ -2,18 +2,15 @@ declare namespace Card {
   type Color = 'red' | 'black';
 
   type Text<T extends Lang = 'en'> =
-    T extends 'fr' ?
-    'as' | 'un' | 'deux' | 'trois' | 'quatre' | 'cinq' | 'six' | 'sept' | 'huit' | 'neuf' | 'dix' | 'valet' | 'dame' | 'roi' | 'joker' :
+    T extends 'fr' ? 'as' | 'un' | 'deux' | 'trois' | 'quatre' | 'cinq' | 'six' | 'sept' | 'huit' | 'neuf' | 'dix' | 'valet' | 'dame' | 'roi' | 'joker' :
     'ace' | 'two' | 'three' | 'four' | 'five' | 'six' | 'seven' | 'eight' | 'nine' | 'ten' | 'jack' | 'queen' | 'king' | 'joker';
 
   type Symbol<T extends Lang = 'en'> =
-    T extends 'fr' ?
-    'A' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'V' | 'D' | 'R' :
+    T extends 'fr' ? 'A' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'V' | 'D' | 'R' :
     'A' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K';
 
   type Suit<T extends Lang = 'en'> =
-    T extends 'fr' ?
-    'carreaux' | 'coeurs' | 'piques' | 'trèfles' :
+    T extends 'fr' ? 'carreaux' | 'coeurs' | 'piques' | 'trèfles' :
     'diamonds' | 'hearts' | 'spades' | 'clubs';
 
   type SuitSymbol = '♦' | '♥' | '♠' | '♣' | '☆' | '★';
