fc71c64e143353690a0397bc4dd963577abd67ec
Camille HAUSTANT <camillehaustant@gmail.com>
2023-04-10 18:52:21 +0200

    modify type of deck

--- types/Player.d.ts
+++ types/Player.d.ts
@@ -1,8 +1,10 @@
+import { Card } from './Card';
+
 export interface PlayerInfo {
   id: number;
   username: string;
 }
 
 export interface Player extends PlayerInfo {
-  deck: number[];
+  deck: Card['id'][];
 }
