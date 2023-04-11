dc652f318b05c81d6552a65ead42f855f5616ae2
Camille HAUSTANT <camillehaustant@gmail.com>
2023-04-11 17:26:31 +0200

    create middleware to handle handshake

---
+++ src/middlewares/socket.middleware.ts
@@ -0,0 +1,38 @@
+import { Socket } from 'socket.io';
+import { io } from '../index';
+import { ExtendedError } from 'socket.io/dist/namespace';
+import { v4 as uuidv4 } from 'uuid';
+
+type MiddlewareSocket = (socket: Socket, next: (err?: ExtendedError | undefined) => void) => void;
+
+export const handshakeUsername: MiddlewareSocket = (socket, next) => {
+  const username = socket.handshake.auth.username;
+
+  if (!username) {
+    return next(new Error('no username'));
+  }
+
+  socket.data.username = username;
+  next();
+};
+
+export const handshakeRoomId: MiddlewareSocket = async (socket, next) => {
+  const roomId = socket.handshake.auth.roomId;
+
+  if (!roomId) {
+    let tempRoomId = uuidv4();
+    while (true) {
+      const socketsInRoom = await io.in(`room-${tempRoomId}`).fetchSockets();
+      if (socketsInRoom.length === 0) break;
+      tempRoomId = uuidv4();
+    }
+    socket.data.room = `room-${tempRoomId}`;
+    return next();
+  }
+
+  const socketsInRoom = await io.in(`room-${roomId}`).fetchSockets();
+  if (socketsInRoom.length === 0) return next(new Error('This room Id doesn\'t exist'));
+
+  socket.data.room = `room-${roomId}`;
+  next();
+};
