97c3401b81abe3509165d6d56466045515d6edb8
Camille HAUSTANT <camillehaustant@gmail.com>
2023-04-11 17:26:39 +0200

    testing socket.io on progress

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import dotenv from 'dotenv';
-import express, { Express, Request, Response } from 'express';
+import express, { Response } from 'express';
 import http from 'http';
-import { Server as ioServer } from 'socket.io';
-import cors from 'cors';
+import { Server as ioServer, Socket } from 'socket.io';
+import cors, { type CorsOptions } from 'cors';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import routes from './routes/index';
 import errorsLogger from './middlewares/errorsLogger';
 import errorsHandler from './middlewares/errorsHandler';
+import { handshakeRoomId, handshakeUsername } from './middlewares/socket.middleware';
 
 dotenv.config();
 
@@ -15,10 +16,16 @@ const app = express();
 const port: number = parseInt(process.env.PORT as string || '3000', 10);
 app.set('port', port);
 
-const server = http.createServer(app);
-const io = new ioServer(server);
+const corsOption: CorsOptions = {
+    origin: 'http://localhost:3001',
+}
 
-app.use(cors());
+const server = http.createServer(app);
+export const io = new ioServer(server, {
+    cors: corsOption,
+});
+
+app.use(cors(corsOption));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,9 +36,23 @@ app.get('/', (_, res: Response) => {
     res.send('Express server for slapgame');
 });
 
-io.on('connection', (socket) => {
-    console.log('user connected');
-    socket.on('disconnect', () => {
+io.use(handshakeUsername);
+io.use(handshakeRoomId);
+
+io.on('connection', async (socket) => {
+    console.log('user connected:', socket.data.username);
+    const room = socket.data.room;
+    socket.join(room);
+    socket.broadcast.emit('player:new', socket.data.username);
+    socket.emit('moveToLobby', room);
+
+    socket.on('test', () => {
+        console.log('test');
+        socket.to(room).emit('testback');
+    })
+
+    socket.on('disconnect', async () => {
+        socket.leave(room);
         console.log('user disconnected');
     });
 });
